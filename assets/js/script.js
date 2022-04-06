c9cc575d5d7c83fbafa5c18a1a0531b8cecbceb0
ib-1 <ibrahimm.ali812@gmail.com>
2022-04-06 02:57:48 +0000

    finished continue feature in javascript where it resets all values for the next match

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,26 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             let data = this.getAttribute("data-type");
             if (this.getAttribute("data-type") === "rock") {
-                console.log('rock')
                 this.style.border = activeStyle;
                 this.style.color = "green";
                 checkActive(data);
                 return choice = this.getAttribute("data-type");
             } else if (this.getAttribute("data-type") === "paper") {
-                console.log('paper')
                 this.style.border = activeStyle;
                 this.style.color = "green";
                 checkActive(data);
                 return choice = this.getAttribute("data-type");
             } else if (this.getAttribute("data-type") === "scissors") {
-                console.log('scissors')
                 this.style.border = activeStyle;
                 this.style.color = "green";
                 checkActive(data);
                 return choice = this.getAttribute("data-type");
             } else if (this.getAttribute("data-type") === "lizard") {
-                console.log('lizard')
                 this.style.border = activeStyle;
                 this.style.color = "green";
                 checkActive(data);
                 return choice = this.getAttribute("data-type");
             } else if (this.getAttribute("data-type") === "spock") {
-                console.log('spock')
                 this.style.border = activeStyle;
                 this.style.color = "green";
                 checkActive(data);
@@ -48,12 +43,14 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-
+    /**
+     * This function loops through all the buttons to check which one is active and remove any styles added
+     * from the ones that arnt active to show it is not active 
+     */
     function checkActive(data) {
         let buttons = document.getElementsByTagName("button");
             for (let button of buttons) {
                 if (button.style.border = "10px solid green" && data === button.getAttribute("data-type")) {
-                
             } else {
                 button.style.border = "none";
                 button.style.color = "black";
@@ -61,7 +58,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 })
-
+/**
+ * this function generates a random number between 1-5 and assings it a value
+ * from rock paper scissors lizard spock which will then be the computers guess
+ * 
+ */
 function computerChoice() {
     let computer = document.getElementById("computer");
     let randomNumber = Math.floor((Math.random() * 5) + 1);
@@ -76,14 +77,17 @@ function computerChoice() {
         return computerGuess = "scissors";
     } else if (randomNumber === 4) {
         computer.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
-        return computerGuess = "liazrd";
+        return computerGuess = "lizard";
     } else if (randomNumber === 5) {
         computer.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
         return computerGuess = "spock";
     }
     
 }
-
+/**
+ * this function rusn both the value of the users choice and the computers choice
+ * and compares them together to see who will win
+ */
 function runGame(choice) {
     let winner = document.getElementById("winner");
     if (choice === "rock" && computerGuess === "scissors") {
@@ -98,9 +102,9 @@ function runGame(choice) {
         winner.innerHTML = "You win!";
     } else if (choice === "scissors" && computerGuess === "lizard") {
         winner.innerHTML = "You win!";
-    } else if (choice === "Lizard" && computerGuess === "paper") {
+    } else if (choice === "lizard" && computerGuess === "paper") {
         winner.innerHTML = "You win!";
-    } else if (choice === "Lizard" && computerGuess === "spock") {
+    } else if (choice === "lizard" && computerGuess === "spock") {
         winner.innerHTML = "You win!";
     } else if (choice === "spock" && computerGuess === "scissors") {
         winner.innerHTML = "You win!";
@@ -111,9 +115,33 @@ function runGame(choice) {
     } else {
         winner.innerHTML = "Computer wins!";
     }
+    restartGame();
 }
 
+/**
+ * this function will replace the submit button with a continue button so the player 
+ * can play the game again
+ */
 function restartGame() {
-    
+    let submitBtn = document.getElementById("submit-button");
+    let continueBtn = document.getElementById("continue-button");
+    continueBtn.style.display = "inline";
+    submitBtn.style.display = "none";
+    continueBtn.addEventListener("click", function() {
+        let computer = document.getElementById("computer");
+        let winner = document.getElementById("winner");
+        let buttons = document.getElementsByTagName("button");
+        for (let button of buttons) {
+            if (button.style.border = "10px solid green") {
+                button.style.border = "none";
+                button.style.color = "black";
+            }
+        }
+        computer.innerHTML = `<i class="fa-solid fa-question"></i>`;
+        winner.innerHTML = "-";
+        continueBtn.style.display = "none";
+        submitBtn.style.display = "inline";
+        return choice = "";
+    })
 }
 
